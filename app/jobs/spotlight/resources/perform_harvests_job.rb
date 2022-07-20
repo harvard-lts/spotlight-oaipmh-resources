deb89e71c8c016ab717154a2da15873492ea7a08
Benjamin Kiah Stroud <kiah@notch8.com>
2022-07-20 10:08:58 -0700

    extract item harvesting logic, refactor

--- app/jobs/spotlight/resources/perform_harvests_job.rb
+++ app/jobs/spotlight/resources/perform_harvests_job.rb
@@ -11,7 +11,7 @@ module Spotlight::Resources
     include Spotlight::JobTracking
     queue_as :default
 
-    attr_reader :harvester, :exhibit, :set, :user
+    attr_reader :harvester, :exhibit, :set, :user, :oai_mods_converter
 
     with_job_tracking(
       resource: ->(job) { job.arguments.first.dig(:harvester) },
@@ -24,6 +24,7 @@ module Spotlight::Resources
       @exhibit = harvester.exhibit
       @set = harvester.data[:set]
       @user = user
+      @oai_mods_converter = OaipmhModsConverter.new(set, exhibit.slug, mapping_file)
 
       harvest_result = harvest(harvester)
       raise HarvestingFailedException if harvest_result[:total_errors].positive?
@@ -42,55 +43,55 @@ module Spotlight::Resources
     end
 
     def harvest(harvester)
-      @oai_mods_converter = OaipmhModsConverter.new(set, exhibit.slug, mapping_file)
-
       harvests = harvester.oaipmh_harvests
       resumption_token = harvests.resumption_token
       last_page_evaluated = false
-      total_items = 0
-      total_errors = 0
-      errored_ids = []
-      until (resumption_token.nil? && last_page_evaluated)
-        #once we reach the last page
-        if (resumption_token.nil?)
-          last_page_evaluated = true
-        end
+      @total_items = 0
+      @total_errors = 0
+      @errored_ids = []
+      until resumption_token.nil? && last_page_evaluated
+        last_page_evaluated = true if resumption_token.nil? # we've reached the last page
 
         harvests.each do |record|
-          @item = OaipmhModsItem.new(exhibit, @oai_mods_converter)
-
-          @item.metadata = record.metadata
-          @item.parse_mods_record
-          begin
-            @item_solr = @item.to_solr
-            @item_sidecar = @item.sidecar_data
-
-            @item.parse_subjects
-            @item.parse_types
-            repository_field_name = @oai_mods_converter.get_spotlight_field_name("repository_ssim")
-            @item.process_images
-            @item.uniquify_repos(repository_field_name)
-
-            # Add clean resource for editing
-            new_resource = OaiUpload.find_or_create_by(exhibit: exhibit, external_id: @item.id) do |new_r|
-              new_r.data = @item_sidecar
-            end
-            new_resource.reindex_later
-            total_items += 1
-          rescue Exception => e
-            Delayed::Worker.logger.add(Logger::ERROR, @item.id + ' did not index successfully')
-            Delayed::Worker.logger.add(Logger::ERROR, e.message)
-            Delayed::Worker.logger.add(Logger::ERROR, e.backtrace)
-            total_errors += 1
-            errored_ids << @item.id
-          end
+          harvest_item(record)
         end
-        if (!resumption_token.nil?)
+
+        if !resumption_token.nil?
           harvests = harvester.resumption_oaipmh_harvests(resumption_token)
           resumption_token = harvests.resumption_token
         end
       end
-      { total_items: total_items, total_errors: total_errors, errored_ids: errored_ids }
+      { total_items: @total_items, total_errors: @total_errors, errored_ids: @errored_ids }
+    end
+
+    def harvest_item(record)
+      item = OaipmhModsItem.new(exhibit, oai_mods_converter)
+
+      item.metadata = record.metadata
+      item.parse_mods_record
+      begin
+        item_solr = item.to_solr
+        item_sidecar = item.sidecar_data
+
+        item.parse_subjects
+        item.parse_types
+        repository_field_name = oai_mods_converter.get_spotlight_field_name('repository_ssim')
+        item.process_images
+        item.uniquify_repos(repository_field_name)
+
+        # Add clean resource for editing
+        new_resource = OaiUpload.find_or_create_by(exhibit: exhibit, external_id: item.id) do |new_r|
+          new_r.data = item_sidecar
+        end
+        new_resource.reindex_later
+        @total_items += 1
+      rescue Exception => e
+        Delayed::Worker.logger.add(Logger::ERROR, item.id + ' did not index successfully')
+        Delayed::Worker.logger.add(Logger::ERROR, e.message)
+        Delayed::Worker.logger.add(Logger::ERROR, e.backtrace)
+        @total_errors += 1
+        @errored_ids << item.id
+      end
     end
 
     def mapping_file
