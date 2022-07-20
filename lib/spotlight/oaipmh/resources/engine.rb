995110045fd7eb5b061abdff2ead7a2decc7b8d5
Rob Kaufman <rob@notch8.com>
2022-07-19 22:38:22 -0700

    get migration to work

--- lib/spotlight/oaipmh/resources/engine.rb
+++ lib/spotlight/oaipmh/resources/engine.rb
@@ -3,7 +3,15 @@ require 'spotlight/engine'
 module Spotlight::Oaipmh::Resources
   class Engine < ::Rails::Engine
     Spotlight::Oaipmh::Resources::Engine.config.resource_partial = 'spotlight/resources/oaipmh_harvester/form'
-    
+
+    initializer :append_migrations do |app|
+      if !app.root.to_s.match(root.to_s) && app.root.join('db/migrate').children.none? { |path| path.fnmatch?("*.spotlight-oaipmh-resources.rb") }
+        config.paths["db/migrate"].expanded.each do |expanded_path|
+          app.config.paths["db/migrate"] << expanded_path
+        end
+      end
+    end
+
     initializer 'spotlight.oaipmh.initialize' do
       Spotlight::Engine.config.external_resources_partials ||= []
       Spotlight::Engine.config.external_resources_partials << Spotlight::Oaipmh::Resources::Engine.config.resource_partial
