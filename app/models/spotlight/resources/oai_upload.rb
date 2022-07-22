7f193cbccb46e4f6ed6c0df7144469cc3efc46a7
Rob Kaufman <rob@notch8.com>
2022-07-22 15:42:25 -0700

    fix file attachment code after refactor

--- app/models/spotlight/resources/oai_upload.rb
+++ app/models/spotlight/resources/oai_upload.rb
@@ -5,6 +5,17 @@ module Spotlight
     ##
     # Exhibit-specific resources, created using uploaded and custom fields
     class OaiUpload < Spotlight::Resources::Upload
+      def attach_image
+        return if self.data['full_image_url_ssm'].blank?
+        image = self.upload || self.create_upload
+        image.remote_image_url = self.data['full_image_url_ssm']
+        iiif_tilesource = riiif.info_path(image)
+        image.update(iiif_tilesource: iiif_tilesource)
+      end
+
+      def riiif
+        Riiif::Engine.routes.url_helpers
+      end
     end
   end
 end
