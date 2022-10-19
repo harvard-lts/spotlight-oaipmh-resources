e017ee185f934b89b359ae5916d28d5853f5a369
Benjamin Kiah Stroud <kiah@notch8.com>
2022-10-19 16:53:44 -0700

    create SolrUpload model
    This will be the SQL copy of items harvested by the SolrHarvester

---
+++ app/models/spotlight/resources/solr_upload.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Spotlight
+  module Resources
+    ##
+    # Exhibit-specific resources, created using uploaded and custom fields
+    class SolrUpload < Spotlight::Resources::Upload; end
+  end
+end
