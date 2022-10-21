00561a59d2c5be3bc8279d32756cd65b510dc295
Benjamin Kiah Stroud <kiah@notch8.com>
2022-10-20 18:10:43 -0700

    override SolrUpload#compound_id
    This allows us to connect URNs with the correct SolrDocuments

--- app/models/spotlight/resources/solr_upload.rb
+++ app/models/spotlight/resources/solr_upload.rb
@@ -4,6 +4,11 @@ module Spotlight
   module Resources
     ##
     # Exhibit-specific resources, created using uploaded and custom fields
-    class SolrUpload < Spotlight::Resources::Upload; end
+    class SolrUpload < Spotlight::Resources::Upload
+      # this sets the path for Solr Uploads from the Harvester to include the URN (aka unique-id) as opposed to the spotlight generated ID. external_id already contains the exhibit_id at the beginning.
+      def compound_id
+        "#{external_id}"
+      end
+    end
   end
 end
