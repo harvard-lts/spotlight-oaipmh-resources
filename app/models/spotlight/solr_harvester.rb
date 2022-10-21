0c6c2c7c26c9fca2f9f3e94837c562cc936dbf2a
Benjamin Kiah Stroud <kiah@notch8.com>
2022-10-20 17:23:47 -0700

    extract single item harvesting logic to SolrHarvester#harvest_item

--- app/models/spotlight/solr_harvester.rb
+++ app/models/spotlight/solr_harvester.rb
@@ -13,7 +13,6 @@ module Spotlight
 
     def harvest_items(job_tracker: nil, job_progress: nil)
       self.total_errors = 0
-      solr_converter = SolrConverter.new(set, exhibit.slug, get_mapping_file)
       solr_converter.parse_mapping_file(solr_converter.mapping_file) 
       page = 1
       harvests = solr_harvests(page)
@@ -26,29 +25,8 @@ module Spotlight
       last_page_evaluated = harvests['response']['docs'].blank?
       while (!last_page_evaluated)
         harvests['response']['docs'].each do |record|
-          parsed_solr_item = SolrHarvestingParser.new(exhibit, solr_converter)
-
-          parsed_solr_item.metadata = record
-          parsed_solr_item.parse_record(unique_id_field)
-          parsed_solr_item.to_solr
-          begin
-            # Create clean resource for editing
-            resource = Spotlight::Resources::SolrUpload.find_or_initialize_by(exhibit: exhibit, external_id: parsed_solr_item.id.upcase)
-            resource.data = parsed_solr_item.sidecar_data
-            resource.save_and_index
-
-            job_progress&.increment
-          rescue Exception => e
-            Delayed::Worker.logger.add(Logger::ERROR, parsed_solr_item.id + ' did not index successfully')
-            Delayed::Worker.logger.add(Logger::ERROR, e.message)
-            Delayed::Worker.logger.add(Logger::ERROR, e.backtrace)
-            if job_tracker.present?
-              job_tracker.append_log_entry(type: :error, exhibit: exhibit, message: error_msg)
-              job_tracker.append_log_entry(type: :error, exhibit: exhibit, message: e.message)
-            end
-            self.total_errors += 1
-          end
-        end # End of each loop
+          harvest_item(record, job_tracker, job_progress)
+        end
 
         page += 1
         unless last_page_evaluated 
@@ -63,7 +41,31 @@ module Spotlight
         if (job_progress.progress % 100).zero?
           job_tracker.append_log_entry(type: :info, exhibit: exhibit, message: "#{job_progress.progress} of #{job_progress.total} (#{self.total_errors} errors)")
         end
-      end # End of while loop
+      end
+    end
+
+    def harvest_item(record, job_tracker, job_progress)
+      parsed_solr_item = SolrHarvestingParser.new(exhibit, solr_converter)
+
+      parsed_solr_item.metadata = record
+      parsed_solr_item.parse_record(unique_id_field)
+      parsed_solr_item.to_solr
+
+      # Create clean resource for editing
+      resource = Spotlight::Resources::SolrUpload.find_or_initialize_by(exhibit: exhibit, external_id: parsed_solr_item.id.upcase)
+      resource.data = parsed_solr_item.sidecar_data
+      resource.save_and_index
+
+      job_progress&.increment
+    rescue Exception => e
+      Delayed::Worker.logger.add(Logger::ERROR, parsed_solr_item.id + ' did not index successfully')
+      Delayed::Worker.logger.add(Logger::ERROR, e.message)
+      Delayed::Worker.logger.add(Logger::ERROR, e.backtrace)
+      if job_tracker.present?
+        job_tracker.append_log_entry(type: :error, exhibit: exhibit, message: error_msg)
+        job_tracker.append_log_entry(type: :error, exhibit: exhibit, message: e.message)
+      end
+      self.total_errors += 1
     end
 
     def solr_harvests(page = nil)
@@ -78,6 +80,10 @@ module Spotlight
     def solr_connection
       solr_url = base_url + set
       @solr_connection ||= RSolr.connect(url: solr_url)
+    end
+
+    def solr_converter
+      @solr_converter ||= SolrConverter.new(set, exhibit.slug, get_mapping_file)
     end
 
     def get_unique_id_field_name(mapping_file)
