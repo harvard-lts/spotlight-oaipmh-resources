1d7e65181411ed97d820c27ca7bec739efbb4406
Benjamin Kiah Stroud <kiah@notch8.com>
2022-10-20 17:14:30 -0700

    remove unused cursor/schedule logic
    Before, every "row" of solr data was harvested in a separate job. Due to how JobTrackers work in Spotlight v3.3.0 (one tracker per job), we run the whole solr harvest in a single PerformHarvestJob

--- app/models/spotlight/solr_harvester.rb
+++ app/models/spotlight/solr_harvester.rb
@@ -12,7 +12,7 @@ module Spotlight
     end
 
     def solr_harvests(page = nil)
-      page = page.present? ? page : 0
+      page = page.present? ? page : 1
       solr_connection.paginate(page, ROW_COUNT, 'select', params: { q: '*:*', wt: 'json' })
     end
 
@@ -27,27 +27,13 @@ module Spotlight
 
     def harvest_items(job_tracker: nil, job_progress: nil)
       self.total_errors = 0
-      max_batch_count = Spotlight::Oaipmh::Resources::Engine.config.solr_harvest_batch_max
       solr_converter = SolrConverter.new(set, exhibit.slug, get_mapping_file)
       solr_converter.parse_mapping_file(solr_converter.mapping_file) 
+      page = 1
+      harvests = solr_harvests(page)
       unique_id_field = nil 
       if (!solr_converter.get_unique_id_field.nil?)
         unique_id_field = solr_converter.get_unique_id_field
-      end
-      count = 0
-      totalrecords = 0
-
-      #If the resumption token was stored, begin there.
-      if !cursor.blank?
-        page = cursor
-        harvests = solr_harvests(page)
-      else
-        page = 1
-        harvests = solr_harvests
-      end
-
-      if !count.blank?
-        totalrecords = count
       end
 
       update_progress_total(job_progress)
@@ -65,8 +51,6 @@ module Spotlight
             resource.data = parsed_solr_item.sidecar_data
             resource.save_and_index
 
-            count = count + 1
-            totalrecords = totalrecords + 1
             job_progress&.increment
           rescue Exception => e
             Delayed::Worker.logger.add(Logger::ERROR, parsed_solr_item.id + ' did not index successfully')
@@ -78,28 +62,22 @@ module Spotlight
             end
             self.total_errors += 1
           end
-        end #End of each loop
-        page = page + 1
-        #Stop harvesting if the batch has reached the maximum allowed value
-        if (!last_page_evaluated) 
-          if (max_batch_count != -1 && count >= max_batch_count)
-            schedule_next_batch(page, totalrecords)
-            break
-          else
-            harvests = solr_harvests(page)
-            update_progress_total(job_progress) # set size can change mid-harvest
-          end
+        end # End of each loop
+
+        page += 1
+        unless last_page_evaluated 
+          harvests = solr_harvests(page)
+          update_progress_total(job_progress) # set size can change mid-harvest
         end
 
-        #Terminate the loop if it is empty        
-        if (harvests['response']['docs'].blank?)
-          last_page_evaluated = true
-        end
+        # Terminate the loop if it is empty
+        last_page_evaluated = true if harvests['response']['docs'].blank?
+
         # Log an update every 100 records
         if (job_progress.progress % 100).zero?
           job_tracker.append_log_entry(type: :info, exhibit: exhibit, message: "#{job_progress.progress} of #{job_progress.total} (#{self.total_errors} errors)")
         end
-      end #End of while loop
+      end # End of while loop
     end
 
     def get_unique_id_field_name(mapping_file)
@@ -109,10 +87,6 @@ module Spotlight
           raise InvalidMappingFile, "spotlight-field is required for each entry"
         end
       end
-    end
-
-    def schedule_next_batch(cursor, count)
-      Spotlight::Resources::PerformHarvestsJob.perform_later(type, base_url, set, mapping_file, exhibit, user, job_entry, cursor, count)
     end
   end
 end
